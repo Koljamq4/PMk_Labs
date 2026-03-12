e9d956b76e4b49234d1504f2b510519ef20f8775
Koljamq4 <168478510+Koljamq4@users.noreply.github.com>
2026-03-12 18:31:25 +0100

    Update main.c

--- lab01_Annissimov/task5/main.c
+++ lab01_Annissimov/task5/main.c
@@ -4,40 +4,33 @@ int main(void)
 {
     CyGlobalIntEnable;
 
-    SW_Tx_UART_Start();
-
-    SW_Tx_UART_PutCRLF();
-    SW_Tx_UART_PutString("Software Transmit UART");
-    SW_Tx_UART_PutCRLF();
-
-    SW_Tx_UART_PutString("Student: Nikolai Anissimov");
-    SW_Tx_UART_PutCRLF();
-
-    uint8_t last_state = 0;
-
     for(;;)
     {
-        if(Button_Read() == 1 && last_state == 0)
+        if(Button_Read() == 0)   // button pressed
         {
-            last_state = 1;
-
-            SW_Tx_UART_PutString("Button released");
-            SW_Tx_UART_PutCRLF();
-
+            // WHITE
             LED_R_Write(0);
             LED_G_Write(0);
             LED_B_Write(0);
-        }
-        else if(Button_Read() == 0 && last_state == 1)
-        {
-            last_state = 0;
-
-            SW_Tx_UART_PutString("Button pressed");
-            SW_Tx_UART_PutCRLF();
+            CyDelay(500);
 
             LED_R_Write(1);
             LED_G_Write(1);
             LED_B_Write(1);
+            CyDelay(500);
+        }
+        else   // button released
+        {
+            // YELLOW
+            LED_R_Write(0);
+            LED_G_Write(0);
+            LED_B_Write(1);
+            CyDelay(500);
+
+            LED_R_Write(1);
+            LED_G_Write(1);
+            LED_B_Write(1);
+            CyDelay(500);
         }
     }
 }
