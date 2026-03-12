e3a07b559c5a583922c0a00533411277a44fb80b
Koljamq4 <168478510+Koljamq4@users.noreply.github.com>
2026-03-12 18:05:51 +0100

    Update main.c

--- lab01_Annissimov/task5/main.c
+++ lab01_Annissimov/task5/main.c
@@ -1,1 +1,43 @@
+#include "project.h"
 
+int main(void)
+{
+    CyGlobalIntEnable;
+
+    SW_Tx_UART_Start();
+
+    SW_Tx_UART_PutCRLF();
+    SW_Tx_UART_PutString("Software Transmit UART");
+    SW_Tx_UART_PutCRLF();
+
+    SW_Tx_UART_PutString("Student: Nikolai Anissimov");
+    SW_Tx_UART_PutCRLF();
+
+    uint8_t last_state = 0;
+
+    for(;;)
+    {
+        if(Button_Read() == 1 && last_state == 0)
+        {
+            last_state = 1;
+
+            SW_Tx_UART_PutString("Button released");
+            SW_Tx_UART_PutCRLF();
+
+            LED_R_Write(0);
+            LED_G_Write(0);
+            LED_B_Write(0);
+        }
+        else if(Button_Read() == 0 && last_state == 1)
+        {
+            last_state = 0;
+
+            SW_Tx_UART_PutString("Button pressed");
+            SW_Tx_UART_PutCRLF();
+
+            LED_R_Write(1);
+            LED_G_Write(1);
+            LED_B_Write(1);
+        }
+    }
+}
